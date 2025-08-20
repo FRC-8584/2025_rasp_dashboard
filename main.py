53b3e341e36924ccd4762bab8a21f5ced918bb05
neoxy <neox9487@gmail.com>
2025-08-21 04:19:00 +0800

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from networktables import NetworkTables
 import uvicorn
 import json
 import asyncio
-
+from contextlib import asynccontextmanager
 from modules import Gemini
 from configs import ALLOWED_ORIGINS, HOST, PORT
 
@@ -12,7 +12,29 @@ Gemini()
 NetworkTables.initialize(server='10.0.0.5')
 table = NetworkTables.getTable('gemini')
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    gemini = Gemini()
+
+    stop_event = asyncio.Event()
+
+    async def update_table():
+        while not stop_event.is_set():
+            status = gemini.get_current_status()
+            table.putNumber('x', status.t_x)
+            table.putNumber('y', status.t_y)
+            await asyncio.sleep(0.05)
+
+    task = asyncio.create_task(update_table())
+
+    yield 
+
+    stop_event.set()
+    await task
+
+app = FastAPI(lifespan=lifespan)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
@@ -23,15 +45,17 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"message": "device server working normally", "gemini_status": Gemini().get_current_status().model_dump_json()}
+    gemini = Gemini()
+    return {"message": "device server working normally", "gemini_status": gemini.get_current_status().model_dump_json()}
 
 @app.get("/gemini/get_status")
 async def get_status(websocket: WebSocket):
     try:
         websocket.accept()
         print("websocket /gemini/get_status connected")
+        gemini = Gemini()
         while True:
-            status = Gemini().get_current_status()
+            status = gemini.get_current_status()
             websocket.send_text(json.dumps(status.model_dump_json()))
             asyncio.wait(0.01)
     except WebSocketDisconnect:
