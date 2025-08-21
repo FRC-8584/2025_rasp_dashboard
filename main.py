2184eb9fca250c75cafe7d97c2ff67006a78abff
neoxy <neox9487@gmail.com>
2025-08-21 11:57:02 +0800

    update

--- main.py
+++ main.py
@@ -12,11 +12,10 @@ Gemini()
 NetworkTables.initialize(server='10.0.0.5')
 table = NetworkTables.getTable('gemini')
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     gemini = Gemini()
-
+    
     stop_event = asyncio.Event()
 
     async def update_table():
@@ -24,17 +23,15 @@ async def lifespan(app: FastAPI):
             status = gemini.get_current_status()
             table.putNumber('x', status.t_x)
             table.putNumber('y', status.t_y)
-            await asyncio.sleep(0.05)
+            await asyncio.sleep(0.01)
 
     task = asyncio.create_task(update_table())
-
     yield 
 
     stop_event.set()
     await task
 
 app = FastAPI(lifespan=lifespan)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
